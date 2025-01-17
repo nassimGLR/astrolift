ae19f83aaea324d7a8ea93b52a280ed6597781a7
Levy <102899@glr.nl>
2025-01-17 14:32:39 +0000

    timer2

--- scripts/bootup.js
+++ scripts/bootup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Wait for animation to complete
     setTimeout(() => {
         // Replace with your main page URL
-        window.location.href = "../../index.html";
+        window.location.href = "./paginas/menu/index.html";
     }, 3000); // 3 seconds - matches animation duration
 });
 
