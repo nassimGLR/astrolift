4ab19627fe5f487823fc1b9f6be756a06c704310
nassimGLR <101935@glr.nl>
2025-01-24 19:24:32 +0000

    fix

--- scripts/bootup.js
+++ scripts/bootup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Wait for animation to complete
     setTimeout(() => {
         // Replace with your main page URL
-        window.location.href = "./paginas/menu/index.html";
+        window.location.href = "/mission2mars/paginas/menu/index.html";
     }, 3000); // 3 seconds - matches animation duration
 });
 
